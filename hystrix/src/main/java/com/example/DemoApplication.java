fc47270fd336eb098c57c2d33134c3079b4ae83b
Jose Miguel Bataller <jmbataller@gmail.com>
2015-12-21 17:30:15 +0000

    Removed hystrix dashboard

--- hystrix/src/main/java/com/example/DemoApplication.java
+++ hystrix/src/main/java/com/example/DemoApplication.java
@@ -1,17 +1,13 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.example.clients"})
 @EnableHystrix
-@EnableHystrixDashboard
 public class DemoApplication {
 
     public static void main(String[] args) {
